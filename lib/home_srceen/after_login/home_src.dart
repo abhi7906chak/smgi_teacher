5d3cb5c5e56ec7ad1a912ff78e42a61ff451e231
abhi7906chak <abhi7906235584@gmail.com>
2024-02-25 17:43:23 +0530

    notification done(All kind of notification in compleled and worked fine (for android) ).

--- lib/home_srceen/after_login/home_src.dart
+++ lib/home_srceen/after_login/home_src.dart
@@ -25,19 +25,23 @@ class _HomesrcState extends State<Homesrc> {
   final firestore = FirebaseFirestore.instance;
   final ImagePicker picker = ImagePicker();
   final notiTitle = TextEditingController();
-  // final RxString image = "".obs;
   File? image;
-  // String networkImage =
-  //     "https://www.google.com/imgres?imgurl=https%3A%2F%2Fimages.hindustantimes.com%2Frf%2Fimage_size_800x600%2FHT%2Fp2%2F2016%2F02%2F12%2FPictures%2F_e7c37f90-d131-11e5-94bd-a06a76346e8f.jpg&tbnid=QPbeEqwXXb9uTM&vet=12ahUKEwicuv7ZxKCEAxUabGwGHRASA9EQMygEegQIARB4..i&imgrefurl=https%3A%2F%2Fwww.hindustantimes.com%2Fmovie-reviews%2Fdeadpool-review-like-ryan-reynolds-this-movie-is-a-gift-from-god%2Fstory-p1cME8PuYmK8SN7d7zfI4H.html&docid=TDyJjL0fdyIOjM&w=800&h=600&q=deadpool&ved=2ahUKEwicuv7ZxKCEAxUabGwGHRASA9EQMygEegQIARB4";
   var teacherdata = {};
   List name = ["Atendence", "data"];
   bool loding = false;
-  notifications notification = notifications();
+  Notifications notification = Notifications();
   @override
   void initState() {
     super.initState();
-    notification.getPermistion();
-    notification.isTokenRefreash();
+    try {
+      Notifications().getPermistion();
+      Notifications().isTokenRefreash();
+      // Notifications().initLocalnotifiacton(context, );
+      Notifications().friebaseinit();
+    } catch (e) {
+      print(e.toString());
+    }
+
     notification.getToken().then((value) => print("token     " + value));
     getData();
   }
@@ -70,7 +74,7 @@ class _HomesrcState extends State<Homesrc> {
         });
       }
     } catch (e) {
-      // print(e);
+      print(e.toString());
     }
     setState(() {
       loding = false;
--- lib/utils/notificatios.dart
+++ lib/utils/notificatios.dart
@@ -1,7 +1,14 @@
-import 'package:firebase_messaging/firebase_messaging.dart';
+// ignore_for_file: avoid_print
 
-class notifications {
+import 'dart:math';
+
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
+class Notifications {
   FirebaseMessaging massage = FirebaseMessaging.instance;
+  FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+      FlutterLocalNotificationsPlugin();
   Future<void> getPermistion() async {
     NotificationSettings setting = await massage.requestPermission(
         alert: true,
@@ -19,6 +26,61 @@ class notifications {
       print("not granted");
     }
   }
+
+  Future<void> initLocalnotifiacton(RemoteMessage message) async {
+    var androidSetting =
+        const AndroidInitializationSettings("@mipmap/ic_launcher");
+
+    ///same for ios
+    var initSetting = InitializationSettings(
+      android: androidSetting, 
+    );
+    await flutterLocalNotificationsPlugin.initialize(
+      initSetting,
+      onDidReceiveNotificationResponse: (payload) {},
+    );
+    AndroidNotificationChannel androidNotificationChannel =
+        AndroidNotificationChannel(
+      Random.secure().nextInt(10000).toString(),
+      'High Importance Notifications',
+      importance: Importance.max,
+    );
+
+    AndroidNotificationDetails androidNotificationDetails =
+        AndroidNotificationDetails(androidNotificationChannel.id.toString(),
+            androidNotificationChannel.name.toString(),
+            channelDescription: 'your channel description',
+            importance: Importance.high,
+            priority: Priority.high,
+            ticker: 'ticker');
+
+    NotificationDetails notificationDetails =
+        NotificationDetails(android: androidNotificationDetails);
+
+    Future.delayed(
+      Duration.zero,
+      () {
+        flutterLocalNotificationsPlugin.show(
+            0,
+            message.notification!.title.toString(),
+            message.notification!.body.toString(),
+            notificationDetails);
+      },
+    );
+  }
+
+  void friebaseinit() {
+    FirebaseMessaging.onMessage.listen((event) {
+      if (event.notification != null) {
+        print(event.notification!.title.toString());
+        print(event.notification!.body.toString());
+        // shownoti(event);
+        initLocalnotifiacton(event);
+      }
+    });
+  }
+
+  Future<void> shownoti(RemoteMessage message) async {}
 
   Future<String> getToken() async {
     String? token = await massage.getToken();
